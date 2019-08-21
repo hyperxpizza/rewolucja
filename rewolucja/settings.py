77c23b7c6111f1303e51e51278b009f75eaa2cc5
Wojciech Frackowski <wojtek.frackowski@gmail.com>
2019-08-21 22:20:30 +0200

    Update settings.py

--- rewolucja/settings.py
+++ rewolucja/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2h#@do#dgwuh+uidnklqn=-m_rx%4)=x@cg(4xof^_zt+70ec&'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
