f195506dbefd24335657bfb3f8f91ec62dfc29b8
Wojciech Frackowski <wojtek.frackowski@gmail.com>
2019-11-01 12:06:00 +0100

    Update settings.py

--- rewolucja/settings.py
+++ rewolucja/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['46.41.135.147','rewolucja.com.pl']
 
 
 # Application definition
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'rewolucja.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,14 +95,6 @@ DATABASES = {
         'PORT': '',   
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
